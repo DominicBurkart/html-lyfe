3c2d0be67dd108bbb8fcea67f3d77437b8fbec48
Dominic Burkart <1dominicb@gmail.com>
2016-05-20 09:52:39 -0500

    working out layout!
    still to do:
    – figure out scrolling/overflow nonsense on mobile (#blog
    overflow:scroll might help?)

---
+++ DominicBurkart/mobile.js
@@ -0,0 +1,9 @@
+var head = document.head;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	var link = document.createElement('link');
+	link.type = 'text/css';
+	link.rel = 'stylesheet';
+	link.href = 'mobile.css';
+	head.appendChild(link);
+	head.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">';
+}
