5494dde0a7a918bd93fc29caf4fdaacd8e2f0e34
Dominic Burkart <1dominicb@gmail.com>
2016-05-24 18:22:01 -0500

    cleaning up!
    removed unnecessary divs around iframes. I don’t think i’ll color code
    this site, so I’ll remove that extraneous tagging in the shared css and
    the html for the main menu

--- DominicBurkart/mobile.js
+++ DominicBurkart/mobile.js
@@ -1,9 +1,21 @@
+var head = document.head;
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	var head = document.head;
+	
 	var link = document.createElement('link');
 	link.type = 'text/css';
 	link.rel = 'stylesheet';
 	link.href = 'mobile.css';
 	head.appendChild(link);
+	
 	head.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">';
+	
+}
+else{
+	if (document.getElementById('blog') !== null){
+		var link = document.createElement('link');
+		link.type = 'text/css';
+		link.rel = 'stylesheet';
+		link.href = 'noScroll.css';
+		head.appendChild(link);
+	} 
 }
